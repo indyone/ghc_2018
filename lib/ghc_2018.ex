df5a14614386aa6d9147c5fd3ce56c50ab1f77e8
Ioannis Stavrinos <diyneon@gmail.com>
2018-03-01 19:25:46 +0200

    Initial commit

---
+++ lib/ghc_2018.ex
@@ -0,0 +1,18 @@
+defmodule Ghc2018 do
+  @moduledoc """
+  Documentation for Ghc2018.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Ghc2018.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ghc_2018_test.exs
@@ -0,0 +1,8 @@
+defmodule Ghc2018Test do
+  use ExUnit.Case
+  doctest Ghc2018
+
+  test "greets the world" do
+    assert Ghc2018.hello() == :world
+  end
+end
